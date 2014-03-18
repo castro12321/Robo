d49935b785e877a6dc8a2db331c4c150894c793a
castro12321 <norbertkawinski@gmail.com>
2014-03-18 06:31:07 +0100

    Better window resizing step

--- src/com/iborg/robo/client/RoboClientWindow.java
+++ src/com/iborg/robo/client/RoboClientWindow.java
@@ -19,17 +19,14 @@
 
 package com.iborg.robo.client;
 
-import java.awt.AWTEvent;
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
-import java.awt.event.AWTEventListener;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
@@ -38,118 +35,124 @@ import javax.swing.JFrame;
 
 import com.iborg.hsocket.ISocket;
 
+class RoboJFrame extends JFrame
+{
+    private static final long serialVersionUID = 1L;
+	private final RoboClientWindow roboWindow;
+	
+	RoboJFrame(RoboClientWindow roboWindow)
+	{
+		this.roboWindow = roboWindow;
+	}
+	
+	@Override
+	public void paint(Graphics g)
+	{
+		super.paint(g);
+		roboWindow.resizeDone();
+	}
+}
+
 public class RoboClientWindow
 {
-	private JFrame resizeBorder = new JFrame();
-	private JFrame window       = new JFrame();
-	private Character resizing        = null;
+	private JFrame window      = new RoboJFrame(this);
+	protected final float sizeRatio;
+	protected final Rectangle originalSize;
+	protected       Rectangle oldSize;
+	private         boolean   resizing;
+	
 	
 	public RoboClientWindow(final RoboClient roboclient, final ISocket socket)
 	{
-		resizeBorder.getContentPane().setBackground(Color.WHITE);
-		resizeBorder.setVisible(false);
-		
+		// Create main window
 		window.setLayout(new BorderLayout());
 		window.add(BorderLayout.CENTER, roboclient.screenCanvas);
-		
-		// Create frame
 		Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
 		final int sizeX = 800, sizeY = 600;
 		final int locationX = (screenDimension.width / 2 - sizeX / 2), locationY = (screenDimension.height / 2 - sizeY / 2);
 		window.setSize(sizeX, sizeY);
 		window.setLocation(locationX, locationY);
 		window.setVisible(true);
-		
-		// Global mouse listener
-		final Toolkit toolkit = Toolkit.getDefaultToolkit();
-		final AWTEventListener listener = new AWTEventListener()
-		{
-			public void eventDispatched(AWTEvent event)
-			{
-				if(event.getID() == MouseEvent.MOUSE_MOVED)
-				{
-					window.setBounds(resizeBorder.getBounds());
-					window.setVisible(true);
-					resizeBorder.setVisible(false);
-					resizing = null;
-				}
-			}
-		};
-		toolkit.addAWTEventListener(listener, AWTEvent.MOUSE_MOTION_EVENT_MASK);
-		
-		
-		// Window listener
 		window.addWindowListener(new WindowAdapter()
-		{
-			@Override
-			public void windowClosing(WindowEvent e)
+		{	@Override public void windowClosing(WindowEvent e)
 			{
-				try
-				{
-					socket.close();
-				}
-				catch(IOException ioe)
-				{}
-				toolkit.removeAWTEventListener(listener);
-				window.dispose();
-				window = null;
-				resizeBorder.dispose();
-				resizeBorder = null;
-				roboclient.stop();
+				RoboClientWindow.this.windowClosing(roboclient, socket);
+			}
+		});
+		window.addComponentListener(new ComponentAdapter()
+		{	@Override public void componentResized(ComponentEvent e)
+			{
+				windowResized(e.getComponent());
 			}
 		});
 		
-		// Component Moved/Resized listener
-		window.addComponentListener(new ComponentAdapter()
-		{
-			@Override
-			public void componentMoved(ComponentEvent e)
-			{
-				resizeBorder.setBounds(window.getBounds());
-				// roboclient.screenCanvas.repaint();
-			}
-			
-			int         oldWidth  = window.getWidth();
-			int         oldHeight = window.getHeight();
-			final float ratioX    = (float) oldWidth / (float) oldHeight;
-			final float ratioY    = (float) oldHeight / (float) oldWidth;
-			
-			
-			@Override
-			public void componentResized(ComponentEvent e)
-			{
-				window.setVisible(false);
-				resizeBorder.setVisible(true);
-				
-				Component component = e.getComponent();
-				Rectangle newSize = component.getBounds();
-				RoboClient.log("bound: " + newSize.x + " " + newSize.y + " " + newSize.width + " " + newSize.height);
-				
-				if(resizing == null)
-					if(newSize.width != oldWidth)
-						resizing = 'x';
-					else
-						resizing = 'y';
-					
-				if(resizing == 'x')
-				{
-					oldWidth = newSize.width;
-					newSize.height = (int) ((float) newSize.width * ratioY);
-				}
-				else
-				{
-					newSize.width = (int) ((float) newSize.height * ratioX);
-					oldHeight = newSize.height;
-				}
-				
-				resizeBorder.setBounds(newSize);
-			}
-		});
+		// Calculate main window size
+		originalSize = oldSize = window.getBounds();
+		sizeRatio    = ((float)originalSize.width) / ((float)originalSize.height);
+		RoboClient.log("Original size: " + originalSize.width + " " + originalSize.height + " ratio: " + sizeRatio);
 	}
 	
 	
 	public void close()
 	{
 		window.dispatchEvent(new WindowEvent(window, WindowEvent.WINDOW_CLOSING));
+	}
+	
+	
+	protected void windowClosing(RoboClient robo, ISocket socket)
+	{
+		try
+		{
+			socket.close();
+		}
+		catch(IOException ioe)
+		{}
+		window.dispose();
+		window = null;
+		robo.stop();
+	}
+	
+	
+	protected void windowResized(Component resized)
+	{
+		Rectangle newSize = resized.getBounds();
+		RoboClient.log("last: " + oldSize.width + " " + oldSize.height + " " + resizing);
+		RoboClient.log("bound: " + newSize.x + " " + newSize.y + " " + newSize.width + " " + newSize.height);
+		
+		if(newSize.width != oldSize.width)
+		{
+			resizing = true;
+		}
+		else if(newSize.height != oldSize.height)
+		{
+			resizing = true;
+		}
+		
+		if(resizing == 'x')
+		{
+			oldWidth = newSize.width;
+			newSize.height = (int) ((float) newSize.width * ratioY);
+		}
+		if(resizing == 'y')
+		{
+			newSize.width = (int) ((float) newSize.height * ratioX);
+			oldHeight = newSize.height;
+		}
+		
+		window.setBounds(newSize);
+	}
+	
+	
+	void resizeDone()
+	{
+		if(resizing)
+			;
+		if(resizing != null) // window has been resized
+		{
+			// Take additional actions
+			// resize main window to fit resizeWindow
+			// hide resize window
+			resizing = false;
+		}
 	}
 }
